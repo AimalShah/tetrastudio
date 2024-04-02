1622f4f03c457a1f97d4577a12853112734a739f
aimalshah <aimalshah62@gmail.com>
2024-04-02 20:24:56 +0500

    fixed mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faTiktok } from '@fortawesome/free-brands-svg-icons'
 import { motion } from 'framer-motion'
 import { Link} from 'react-router-dom'
 
@@ -11,12 +14,12 @@ export function MobileNavMenu ({navLinks , handleClick}) {
 
 return (
   <motion.div
-  className=' w-screen h-[90vh] flex flex-col gap-10 fixed bg-white top-20 left-0 z-10 pt-2 shadow-md '
-  initial={{x : 100 , opacity : 0 , clipPath : 'circle(0.4% at 100% 46%)'}}
-  animate={{x:0 , opacity : 1 , clipPath : 'circle(70.7% at 50% 50%)'}}
-  transition={{duration : 0.6}}
+  className='lg:hidden w-screen h-[90vh] flex flex-col gap-10 fixed bg-white top-20 left-0 z-10 pt-2  '
+  initial={{ clipPath : 'circle(0.4% at 100% 0)'}}
+  animate={{clipPath : ' circle(140.7% at 99% 0)'}}
+  transition={{duration : 0.4}}
   >
-    <div className='flex flex-col items-center w-full border'>
+    <div className='flex flex-col items-center w-full'>
       {
         navLinks.map((item) => (
           <h2 
@@ -29,15 +32,15 @@ return (
       }
     </div>
     <div className='text-center mt-4'>
-      <button className='bg-primary px-4 py-2 w-60 rounded-full text-white'>
+      <button className='bg-primary px-4 py-4 w-80 rounded-full text-white font-semibold'>
         Get Quote
       </button>
     </div>
-    <div>
-      <FontAwesomeIcon icon={faSquareInstagram} />
-      <FontAwesomeIcon icon={faSquareInstagram} />
-      <FontAwesomeIcon icon={faSquareInstagram} />
-      <FontAwesomeIcon icon={faSquareInstagram} />
+    <div className='flex items-center justify-center gap-6'>
+      <FontAwesomeIcon icon={faSquareInstagram}  className='size-10 text-gray-600'/>
+      <FontAwesomeIcon icon={faSquareFacebook}  className='size-10 text-gray-600'/>
+      <FontAwesomeIcon icon={faSquareTwitter}  className='size-10 text-gray-600'/>
+      <FontAwesomeIcon icon={faTiktok}  className='size-10 text-gray-600'/>
     </div>
 
   </motion.div>
@@ -49,9 +52,9 @@ export function DesktopNavMenu ({navLinks , handleClick}) {
   return (
   <motion.div
   className='hidden lg:flex w-screen h-[90vh] gap-40 fixed bg-white top-30 left-0 z-10 pt-20 justify-center shadow-md'
-  initial={{x : 100 , opacity : 0 , clipPath : 'circle(0.4% at 100% 46%)'}}
-  animate={{x:0 , opacity : 1 , clipPath : 'circle(70.7% at 50% 50%)'}}
-  transition={{duration : 0.6}}
+  initial={{  clipPath : 'circle(0.4% at 100% 46%)'}}
+  animate={{  clipPath : 'circle(70.7% at 50% 50%)'}}
+  transition={{duration : 0.4}}
   >
     <div className='flex flex-col justify-start '>
       <h1 className='text-xs'>Get in touch</h1>
@@ -129,7 +132,7 @@ function Navbar() {
                 <FontAwesomeIcon icon={faBars}  className='text-white'/>
                } 
             </button>
-            {isActive && <DesktopNavMenu navLinks={navLinks} handleClick={handleClick}/>}
+            {isActive && <DesktopNavMenu navLinks={navLinks} handleClick={handleClick} />}
             {isActive && <MobileNavMenu navLinks={navLinks} handleClick={handleClick}/>}
         </div>
       </div>

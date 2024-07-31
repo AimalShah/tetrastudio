c9974ee3c960fce80228ce9d24f79a5b858cb104
aimalshah <aimalshah62@gamil.com>
2024-07-31 14:27:22 +0500

    fixed navigtion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <h1 className="text-4xl font-semibold text-center mb-6 mt-10">Contact</h1>
       <div className="flex justify-center p-4">
         <button className="hover:bg-accent hover:text-white border-2 border-accent px-8 py-4 rounded-full lg:w-1/2 w-3/4">
-        <a href="mailto:info@tetrastudio.net" className="w-full h-full block">
+        <a href="mailto:info@tetrastudio.net" className="w-full h-full block" target="_blank">
           Get In Touch
         </a>
         </button>
